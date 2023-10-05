5f79b18a67608591bda447af24e77b21737ee99d
whs31 <75864292+whs31@users.noreply.github.com>
2023-10-05 15:24:07 +0300

    Update qtexelevation-algorithms.h

--- libs/elevation/src/c++/qtexelevation-algorithms.h
+++ libs/elevation/src/c++/qtexelevation-algorithms.h
@@ -22,7 +22,7 @@ namespace QtEx
   bool loadTiles(int8_t minLatitude, int16_t minLongitude, int8_t maxLatitude, int16_t maxLongitude);
   bool loadTiles(const QGeoPath & _path);
 
-  int16_t elevation(double latitude, double longitude);
+  int16_t elevation(double latitude, double longitude, PreLoad mode = PreLoad::False);
 
   auto buildProfile(const QGeoPath& path, uint8_t discrete = 10) -> vector<pair<uint32_t, int16_t>>;
   QGeoPath buildProfileAsGeoPath(const QGeoPath& path, float step = 1.f);
