79e2e31d3a0dc858988d8954b2c2b93140c83b7c
whs31 <75864292+whs31@users.noreply.github.com>
2023-10-17 23:33:13 +0300

    Update qtexcore-global.h

--- libs/core/src/c++/qtexcore-global.h
+++ libs/core/src/c++/qtexcore-global.h
@@ -49,10 +49,12 @@ using i8 = int8_t;
 using i16 = int16_t;
 using i32 = int32_t;
 using i64 = int64_t;
+using i64sd = qint64;
 using u8 = uint8_t;
 using u16 = uint16_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
+using u64sd = quint64;
 using usize = size_t;
 using isize = int;
 using uptr = uintptr_t;
@@ -61,6 +63,7 @@ using f32 = float;
 using f64 = double;
 
 #define as_const qAsConst
+#define self (*this)
 
 #if defined(QTEX_CORE_ENABLE_TESTS)
 // if needed
